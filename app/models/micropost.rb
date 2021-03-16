95d76f2906979ff88d742875d57a17344aa516a4
Ruvanthika-thennavan <ruvanthika.thennavan@freshworks.com>
2021-03-16 10:01:49 +0530

    association error

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,7 @@
 class Micropost < ApplicationRecord
+belongs_to :user
 validates :content, length: { maximum: 140 },
                       presence: true
-belongs_to :users, :optional => true
+#belongs_to :users, :optional => true
 end
 
